ed97b5a384cdb324d325dc1980fecf9b592d9c56
Nitesh_Kumar <17niteshkumar17@gmail.com>
2019-09-21 02:20:05 +0530

    code file

---
+++ Word_cloud.R
@@ -0,0 +1,45 @@
+#load data
+library(pdftools)
+data <- pdf_text("nc.pdf")
+
+
+library(tm)
+options(header=F,stringsAsFactors = F,FileEncoding="latin1")
+
+#make corpus
+corpus <- Corpus(VectorSource(data))
+
+#data cleaning
+corpus <- tm_map(corpus,tolower)
+corpus <- tm_map(corpus,removePunctuation)
+corpus <- tm_map(corpus,removeNumbers)
+corpus <- tm_map(corpus,removeWords,stopwords("english"))
+remove_specials <- function(x) gsub("\r?\n|\r"," ",x)
+corpus <- tm_map(corpus,content_transformer(remove_specials))
+corpus <- tm_map(corpus,stripWhitespace)
+#finally clean text
+cleantxt <- tm_map(corpus,PlainTextDocument)
+cleantxt <- Corpus(VectorSource(cleantxt))
+#term document matrix
+tdm <- TermDocumentMatrix(cleantxt,control=list(minWordLength=c(1,Inf)))
+word_mat <- as.matrix(tdm)
+#inspecting frequency words
+findFreqTerms(tdm,lowfreq = 10)
+
+#Bar plot
+term_freq <- rowSums(word_mat)
+term_freq <- subset(term_freq,term_freq>=7)
+library(ggplot2)
+barplot(term_freq,las=2,col=rainbow(20))
+#if any unwanted word comes use tm_map(corpus,removeWords,c("","",""))
+
+#word cloud
+library(wordcloud)
+wordFreq <- sort(rowSums(word_mat),decreasing = T)
+
+grayLevel <- gray((wordFreq+10)/(max(wordFreq)+10))
+
+#1st word cloud with gray level
+wordcloud(words=names(wordFreq),freq=wordFreq,min.freq=5,random.order = F,color=grayLevel)
+#2nd with colors
+wordcloud(words=names(wordFreq),freq=wordFreq,min.freq=5,random.order = F,color=brewer.pal(11,"Paired"))
